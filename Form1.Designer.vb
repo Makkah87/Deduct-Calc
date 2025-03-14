bba1be0e8e8f29aa6ee3c8d0a454ff1a6c80d5ca
Makkah87 <98203855+Makkah87@users.noreply.github.com>
2025-03-14 13:55:09 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_grosspay = New System.Windows.Forms.TextBox()
+        Me.txt_family_leave = New System.Windows.Forms.TextBox()
+        Me.txt_disability = New System.Windows.Forms.TextBox()
+        Me.txt_unemployment = New System.Windows.Forms.TextBox()
+        Me.txt_netpay = New System.Windows.Forms.TextBox()
+        Me.Benefits = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmd_cal = New System.Windows.Forms.Button()
+        Me.cmd_clear = New System.Windows.Forms.Button()
+        Me.cmd_close = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmd_reload = New System.Windows.Forms.Button()
+        Me.cmd_save = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_un_rate = New System.Windows.Forms.TextBox()
+        Me.txt_dis_rate = New System.Windows.Forms.TextBox()
+        Me.txt_fl_rate = New System.Windows.Forms.TextBox()
+        Me.Benefits.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txt_grosspay
+        '
+        Me.txt_grosspay.Location = New System.Drawing.Point(63, 45)
+        Me.txt_grosspay.Name = "txt_grosspay"
+        Me.txt_grosspay.Size = New System.Drawing.Size(82, 20)
+        Me.txt_grosspay.TabIndex = 0
+        '
+        'txt_family_leave
+        '
+        Me.txt_family_leave.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_family_leave.Location = New System.Drawing.Point(17, 54)
+        Me.txt_family_leave.Name = "txt_family_leave"
+        Me.txt_family_leave.ReadOnly = True
+        Me.txt_family_leave.Size = New System.Drawing.Size(82, 20)
+        Me.txt_family_leave.TabIndex = 1
+        '
+        'txt_disability
+        '
+        Me.txt_disability.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_disability.Location = New System.Drawing.Point(134, 54)
+        Me.txt_disability.Name = "txt_disability"
+        Me.txt_disability.ReadOnly = True
+        Me.txt_disability.Size = New System.Drawing.Size(82, 20)
+        Me.txt_disability.TabIndex = 2
+        '
+        'txt_unemployment
+        '
+        Me.txt_unemployment.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_unemployment.Location = New System.Drawing.Point(241, 54)
+        Me.txt_unemployment.Name = "txt_unemployment"
+        Me.txt_unemployment.ReadOnly = True
+        Me.txt_unemployment.Size = New System.Drawing.Size(82, 20)
+        Me.txt_unemployment.TabIndex = 3
+        '
+        'txt_netpay
+        '
+        Me.txt_netpay.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_netpay.Location = New System.Drawing.Point(287, 45)
+        Me.txt_netpay.Name = "txt_netpay"
+        Me.txt_netpay.ReadOnly = True
+        Me.txt_netpay.Size = New System.Drawing.Size(77, 20)
+        Me.txt_netpay.TabIndex = 4
+        '
+        'Benefits
+        '
+        Me.Benefits.Controls.Add(Me.Label5)
+        Me.Benefits.Controls.Add(Me.Label4)
+        Me.Benefits.Controls.Add(Me.Label3)
+        Me.Benefits.Controls.Add(Me.txt_unemployment)
+        Me.Benefits.Controls.Add(Me.txt_disability)
+        Me.Benefits.Controls.Add(Me.txt_family_leave)
+        Me.Benefits.Location = New System.Drawing.Point(46, 88)
+        Me.Benefits.Name = "Benefits"
+        Me.Benefits.Size = New System.Drawing.Size(342, 94)
+        Me.Benefits.TabIndex = 5
+        Me.Benefits.TabStop = False
+        Me.Benefits.Text = "Benefits"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(238, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(93, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "NJ Unemployment"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(131, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "NJ Disability"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "NJ Family Leave"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Gross Pay"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(284, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Net Pay"
+        '
+        'cmd_cal
+        '
+        Me.cmd_cal.Location = New System.Drawing.Point(163, 198)
+        Me.cmd_cal.Name = "cmd_cal"
+        Me.cmd_cal.Size = New System.Drawing.Size(99, 27)
+        Me.cmd_cal.TabIndex = 8
+        Me.cmd_cal.Text = "Calculate"
+        Me.cmd_cal.UseVisualStyleBackColor = True
+        '
+        'cmd_clear
+        '
+        Me.cmd_clear.Location = New System.Drawing.Point(161, 45)
+        Me.cmd_clear.Name = "cmd_clear"
+        Me.cmd_clear.Size = New System.Drawing.Size(67, 20)
+        Me.cmd_clear.TabIndex = 9
+        Me.cmd_clear.Text = "Clear"
+        Me.cmd_clear.UseVisualStyleBackColor = True
+        '
+        'cmd_close
+        '
+        Me.cmd_close.Location = New System.Drawing.Point(317, 461)
+        Me.cmd_close.Name = "cmd_close"
+        Me.cmd_close.Size = New System.Drawing.Size(91, 24)
+        Me.cmd_close.TabIndex = 10
+        Me.cmd_close.Text = "Close"
+        Me.cmd_close.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmd_reload)
+        Me.GroupBox1.Controls.Add(Me.cmd_save)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txt_un_rate)
+        Me.GroupBox1.Controls.Add(Me.txt_dis_rate)
+        Me.GroupBox1.Controls.Add(Me.txt_fl_rate)
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 264)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(366, 181)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Rates"
+        '
+        'cmd_reload
+        '
+        Me.cmd_reload.Location = New System.Drawing.Point(199, 119)
+        Me.cmd_reload.Name = "cmd_reload"
+        Me.cmd_reload.Size = New System.Drawing.Size(99, 22)
+        Me.cmd_reload.TabIndex = 16
+        Me.cmd_reload.Text = "Reload"
+        Me.cmd_reload.UseVisualStyleBackColor = True
+        '
+        'cmd_save
+        '
+        Me.cmd_save.Location = New System.Drawing.Point(70, 119)
+        Me.cmd_save.Name = "cmd_save"
+        Me.cmd_save.Size = New System.Drawing.Size(99, 22)
+        Me.cmd_save.TabIndex = 13
+        Me.cmd_save.Text = "Save"
+        Me.cmd_save.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(253, 42)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(100, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "UnemploymentRate"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(146, 42)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Disability Rate"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 42)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Family Leave Rate"
+        '
+        'txt_un_rate
+        '
+        Me.txt_un_rate.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_un_rate.Location = New System.Drawing.Point(256, 70)
+        Me.txt_un_rate.Name = "txt_un_rate"
+        Me.txt_un_rate.Size = New System.Drawing.Size(82, 20)
+        Me.txt_un_rate.TabIndex = 13
+        '
+        'txt_dis_rate
+        '
+        Me.txt_dis_rate.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_dis_rate.Location = New System.Drawing.Point(149, 70)
+        Me.txt_dis_rate.Name = "txt_dis_rate"
+        Me.txt_dis_rate.Size = New System.Drawing.Size(82, 20)
+        Me.txt_dis_rate.TabIndex = 12
+        '
+        'txt_fl_rate
+        '
+        Me.txt_fl_rate.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txt_fl_rate.Location = New System.Drawing.Point(32, 70)
+        Me.txt_fl_rate.Name = "txt_fl_rate"
+        Me.txt_fl_rate.Size = New System.Drawing.Size(82, 20)
+        Me.txt_fl_rate.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 506)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.cmd_close)
+        Me.Controls.Add(Me.cmd_clear)
+        Me.Controls.Add(Me.cmd_cal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Benefits)
+        Me.Controls.Add(Me.txt_netpay)
+        Me.Controls.Add(Me.txt_grosspay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Benefits Calculator"
+        Me.TopMost = True
+        Me.Benefits.ResumeLayout(False)
+        Me.Benefits.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_grosspay As TextBox
+    Friend WithEvents txt_family_leave As TextBox
+    Friend WithEvents txt_disability As TextBox
+    Friend WithEvents txt_unemployment As TextBox
+    Friend WithEvents txt_netpay As TextBox
+    Friend WithEvents Benefits As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmd_cal As Button
+    Friend WithEvents cmd_clear As Button
+    Friend WithEvents cmd_close As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cmd_reload As Button
+    Friend WithEvents cmd_save As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txt_un_rate As TextBox
+    Friend WithEvents txt_dis_rate As TextBox
+    Friend WithEvents txt_fl_rate As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,218 @@
+﻿Public Class Form1
+    Private Sub cmd_cal_Click(sender As Object, e As EventArgs) Handles cmd_cal.Click
+        Dim gross_pay, net_pay, family_leave, disability, unemployment As Decimal 'Results
+        Dim fl_rate, dis_rate, un_rate As Decimal 'Rates
+        Dim GP_Char1 As String
+
+        GP_Char1 = txt_grosspay.Text 'Check if first character is a decimal point
+
+
+        'Data Input
+        If txt_grosspay.Text <> "" And GP_Char1.Substring(0) <> "." Then 'Only execute if field isn't blank
+            gross_pay = txt_grosspay.Text
+            fl_rate = txt_fl_rate.Text
+            dis_rate = txt_dis_rate.Text
+            un_rate = txt_un_rate.Text
+
+
+
+            'Data Processing
+            family_leave = Math.Truncate((gross_pay * fl_rate) * 100) / 100
+            disability = Math.Truncate((gross_pay * dis_rate) * 100) / 100
+            unemployment = Math.Truncate((gross_pay * un_rate) * 100) / 100
+            net_pay = gross_pay - (family_leave + disability + unemployment)
+
+            If gross_pay > 0 Then 'Only Execute if a value greater than 0 is entered
+
+                'Data Output
+                txt_family_leave.Text = family_leave
+                txt_disability.Text = disability
+                txt_unemployment.Text = unemployment
+                txt_netpay.Text = net_pay
+
+                'Go to Clear Button
+                cmd_clear.Focus()
+            Else
+                MessageBox.Show("Please enter a number greater than 0 into the gross pay field", "WOOPS!")
+                txt_grosspay.Text = ""
+
+            End If
+
+        Else
+            MessageBox.Show("Please enter a number into the gross pay field", "NU-UH!")
+            txt_grosspay.Text = ""
+
+        End If
+
+    End Sub
+
+    Private Sub cmd_clear_Click(sender As Object, e As EventArgs) Handles cmd_clear.Click
+        'Clear All Text Boxes
+        txt_disability.Text = ""
+        txt_family_leave.Text = ""
+        txt_grosspay.Text = ""
+        txt_netpay.Text = ""
+        txt_unemployment.Text = ""
+
+        'Place Cursor Back in Gross Pay Text Box
+        txt_grosspay.Focus()
+
+
+
+    End Sub
+
+    Private Sub cmd_close_Click(sender As Object, e As EventArgs) Handles cmd_close.Click
+        Close()
+    End Sub
+
+    Private Sub txt_grosspay_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_grosspay.KeyPress
+        'Only Accept Numbers
+        'Allow decimal points (ASCII Code 46) and Enter Key (ASCII Code 13)
+        If Asc(e.KeyChar) <> 13 AndAlso Asc(e.KeyChar) <> 8 AndAlso Asc(e.KeyChar) <> 46 AndAlso Asc(e.KeyChar) <> 13 AndAlso Not IsNumeric(e.KeyChar) Then
+            MessageBox.Show("Please enter numbers only", "NOPE!")
+
+            e.Handled = True
+        End If
+        '
+        '
+        '
+        'Enter Key Will Trigger Calculation Function
+        '
+        '
+        '
+        '
+        '
+        If Asc(e.KeyChar) = 13 Then ' Enter Key = 13
+
+
+            Dim gross_pay, net_pay, family_leave, disability, unemployment As Decimal
+            Dim fl_rate, dis_rate, un_rate As Decimal 'Rates
+            Dim GP_Char1 As String
+
+
+            GP_Char1 = txt_grosspay.Text 'Check if first character is a decimal point
+
+
+            'Data Input
+            If txt_grosspay.Text <> "" And GP_Char1.Substring(0) <> "." Then 'Only execute if field isn't blank
+                gross_pay = txt_grosspay.Text
+                fl_rate = txt_fl_rate.Text
+                dis_rate = txt_dis_rate.Text
+                un_rate = txt_un_rate.Text
+
+
+                'Data Processing
+                family_leave = Math.Truncate((gross_pay * fl_rate) * 100) / 100
+                disability = Math.Truncate((gross_pay * dis_rate) * 100) / 100
+                unemployment = Math.Truncate((gross_pay * un_rate) * 100) / 100
+                net_pay = gross_pay - (family_leave + disability + unemployment)
+
+                If gross_pay > 0 Then 'Only Execute if a value greater than 0 is entered
+
+                    'Data Output
+                    txt_family_leave.Text = family_leave
+                    txt_disability.Text = disability
+                    txt_unemployment.Text = unemployment
+                    txt_netpay.Text = net_pay
+
+                    'Go to Clear Button
+                    cmd_clear.Focus()
+                Else
+                    MessageBox.Show("Please enter a number greater than 0 into the gross pay field", "WOOPS!")
+                    txt_grosspay.Text = ""
+
+                End If
+
+            Else
+                MessageBox.Show("Please enter a number into the gross pay field", "NU-UH!")
+                txt_grosspay.Text = ""
+
+            End If
+
+        End If
+
+    End Sub
+
+
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim fl_rate As Decimal
+        Dim dis_rate As Decimal
+        Dim un_rate As Decimal
+
+
+
+        'Load From Text File (In Progress)
+        Dim reader As New System.IO.StreamReader("C:\BC\bc_save.txt")
+        Dim allLines As List(Of String) = New List(Of String)
+        Do While Not reader.EndOfStream
+            allLines.Add(reader.ReadLine())
+        Loop
+        reader.Close()
+        fl_rate = ReadLine(1, allLines)
+        dis_rate = ReadLine(2, allLines)
+        un_rate = ReadLine(3, allLines)
+
+
+
+        'Apply Saved Rate Values to Text Boxes
+        txt_fl_rate.Text = fl_rate
+        txt_dis_rate.Text = dis_rate
+        txt_un_rate.Text = un_rate
+
+    End Sub
+
+    Public Function ReadLine(lineNumber As Integer, lines As List(Of String)) As String
+        Return lines(lineNumber - 1)
+    End Function
+
+    Private Sub cmd_reload_Click(sender As Object, e As EventArgs) Handles cmd_reload.Click
+        Dim fl_rate As Decimal
+        Dim dis_rate As Decimal
+        Dim un_rate As Decimal
+
+
+
+        'Load From Text File
+        Dim reader As New System.IO.StreamReader("C:\BC\bc_save.txt")
+        Dim allLines As List(Of String) = New List(Of String)
+        Do While Not reader.EndOfStream
+            allLines.Add(reader.ReadLine())
+        Loop
+        reader.Close()
+        fl_rate = ReadLine(1, allLines)
+        dis_rate = ReadLine(2, allLines)
+        un_rate = ReadLine(3, allLines)
+
+
+        'Apply Saved Rate Values to Text Boxes
+        txt_fl_rate.Text = fl_rate
+        txt_dis_rate.Text = dis_rate
+        txt_un_rate.Text = un_rate
+    End Sub
+
+    Private Sub cmd_save_Click(sender As Object, e As EventArgs) Handles cmd_save.Click
+        Dim path As String = "C:\BC\bc_save.txt"    '"C:\BC\bc_save.txt"
+
+        'Writing Variables
+        Dim write_fl_rate, write_dis_rate, write_un_rate As Decimal
+
+        'Assign Writing Variables with Text Box Values
+        write_fl_rate = txt_fl_rate.Text
+        write_dis_rate = txt_dis_rate.Text
+        write_un_rate = txt_un_rate.Text
+
+        FileOpen(1, path, 2)
+
+        'Writ to file
+        WriteLine(1, write_fl_rate)
+        WriteLine(1, write_dis_rate)
+        WriteLine(1, write_un_rate)
+
+        FileClose(1)
+
+
+
+    End Sub
+End Class
